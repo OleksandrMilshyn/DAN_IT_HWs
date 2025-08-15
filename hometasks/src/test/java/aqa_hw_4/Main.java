4e7548b3262d783069e77d79db5dc1d96cb1d1da
[username] <[email]>
2025-08-15 16:55:39 +0200

    the first task is over (aqa_hw_4)

--- hometasks/src/test/java/aqa_hw_4/Main.java
+++ hometasks/src/test/java/aqa_hw_4/Main.java
@@ -1,24 +1,37 @@
 package aqa_hw_4;
 
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import static java.lang.String.format;
 import static java.lang.Thread.sleep;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
 
         try {
+            String wordToFind = "Харків";
+
             driver.get("https://hotline.ua/");
             driver.manage().window().maximize();
 
-            WebElement searchButtonLocation = driver.findElement(By.cssSelector("[class='location flex middle-xs']"));
+            WebElement searchButtonLocation = driver.findElement(By.xpath("//div[@class='location flex middle-xs']"));
             searchButtonLocation.click();
             sleep(3000);
+
+            WebElement searchNewLocation = driver.findElement(By.xpath("//span[text()='Харків']"));
+            searchNewLocation.click();
+            sleep(3000);
+
+            WebElement isKharkivLocation = driver.findElement(By.xpath("//div[@title='Харків']"));
+            String pageTitleString = isKharkivLocation.getText();
+
+            Assert.assertTrue(format("<%s> title doesn't contain <%s> word", pageTitleString, wordToFind), pageTitleString.contains(wordToFind));
 
         } catch (AssertionError ex) {
         ex.printStackTrace();
@@ -28,23 +41,3 @@ public class Main {
     }
 }
 
-//WebElement searchField = driver.findElement(By.cssSelector("[type = 'text']"));
-//            searchField.sendKeys(wordToFind);
-//sleep(3000);
-//
-//WebElement searchButton = driver.findElement(By.cssSelector("[title = 'Поиск']"));
-//            searchButton.click();
-//sleep(5000);
-//
-//WebElement titleElement = driver.findElement(By.cssSelector("[class = 'catalog-title__main']"));
-//String pageTitleString = titleElement.getText();
-//
-//            Assert.assertTrue(format("<%s> title doesn't contain <%s> word", pageTitleString, wordToFind), pageTitleString.contains(wordToFind));
-//
-//        } catch (AssertionError ex) {
-//        ex.printStackTrace();
-//        } finally {
-//                driver.quit();
-//        }
-//                }
-//                }
