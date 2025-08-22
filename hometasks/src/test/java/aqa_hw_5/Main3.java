6ced6320a6487ca56cff44110f3789c2813e1c50
[username] <[email]>
2025-08-22 16:23:01 +0200

    task3

---
+++ hometasks/src/test/java/aqa_hw_5/Main3.java
@@ -0,0 +1,42 @@
+package aqa_hw_5;
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+
+public class Main3 {
+
+    public static void main(String[] args) {
+        int items = 48;
+
+        WebDriver driver = new ChromeDriver();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        driver.manage().window().maximize();
+        driver.get("https://hotline.ua/");
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.button-menu-main")));
+        WebElement catalogButton = driver.findElement(By.cssSelector("div.button-menu-main"));
+        catalogButton.click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[data-id='548']")));
+        WebElement householdAppliances = driver.findElement(By.cssSelector("[data-id='548']"));
+        householdAppliances.click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[src='/img/uploads/clients/5e681eea33a67.jpg']")));
+        WebElement washingMachines = driver.findElement(By.cssSelector("[src='/img/uploads/clients/5e681eea33a67.jpg']"));
+        washingMachines.click();
+
+        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.cssSelector("div.list-item")));
+        List<WebElement> itemsOnPage = driver.findElements(By.cssSelector("div.list-item"));
+        Assert.assertEquals(itemsOnPage.size(), items);
+
+        driver.quit();
+    }
+}
