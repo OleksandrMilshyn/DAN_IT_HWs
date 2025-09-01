b9b17e961fdc84e6be87cb20816cff267e326c44
[username] <[email]>
2025-09-01 18:18:55 +0200

    hw_9 task2

---
+++ hometasks/src/main/java/java_hw_9/task2/Main.java
@@ -0,0 +1,8 @@
+package java_hw_9.task2;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ hometasks/src/main/java/java_hw_9/task2/Vehicle.java
@@ -0,0 +1,19 @@
+package java_hw_9.task2;
+
+public class Vehicle {
+
+    private static Vehicle instance;
+
+//    private Vehicle(){
+//        }
+//    public static Vehicle getInstance(){
+//        if instance == null{
+//            instance = new Vehicle();
+//        }
+//        return instance;
+//    }
+//    public String name;
+//    public int speed;
+//    public int price;
+
+}
