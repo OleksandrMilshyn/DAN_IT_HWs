c971388027b9a86bf5df67d3de8ae0d72e1ac0ee
[username] <[email]>
2025-09-04 00:07:28 +0200

    some chenges

---
+++ hometasks/src/test/java/aqa_hw_7/BaseTest.java
@@ -0,0 +1,30 @@
+package aqa_hw_7;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.time.Duration;
+
+public class BaseTest {
+
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void driverInit() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://hotline.ua/");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+    }
+
+    @AfterMethod
+    public void quitDriver(){
+        this.driver.quit();
+    }
+
+    public WebDriver getDriver() {
+        return driver;
+    }
+}
---
+++ hometasks/src/test/java/aqa_hw_7/HomePageTest.java
@@ -0,0 +1,25 @@
+package aqa_hw_7;
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+public class HomePageTest extends BaseTest {
+
+    @Test
+    public void verifyLoginField() {
+
+        WebElement userButton = getDriver().findElement(By.cssSelector("div.user-info"));
+        userButton.click();
+
+        WebElement registrationButton = getDriver().findElement(By.cssSelector("[class='template-page__link']"));
+        registrationButton.click();
+
+        WebElement imgHotLine = getDriver().findElement(By.cssSelector("[src='/frontend/_nuxt/img/logo-smart-shopping.b026975.svg']"));
+        imgHotLine.click();
+
+        String currentUrl = getDriver().getCurrentUrl();
+        Assert.assertEquals("Is not a home page", "https://hotline.ua/", currentUrl);
+    }
+}
