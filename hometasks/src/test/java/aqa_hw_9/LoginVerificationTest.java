564ea1b405f856ae82b06246feda6fd4698cc40c
[username] <[email]>
2025-09-10 00:25:44 +0200

    some changes

---
+++ hometasks/src/test/java/aqa_hw_9/LoginVerificationTest.java
@@ -0,0 +1,22 @@
+package aqa_hw_9;
+
+import org.junit.Assert;
+import org.testng.annotations.Test;
+
+public class LoginVerificationTest extends BaseTest{
+
+    @Test
+    public void verifyLogin(){
+
+        String wordToVerify = "Зміна паролю";
+
+        HomePage homePage = new HomePage(getDriver());
+        homePage.toUserPage();
+
+        LoginPage loginPage = new LoginPage(getDriver());
+
+        loginPage.toForgetPassword();
+
+        Assert.assertEquals(wordToVerify, loginPage.verifyTitle());
+    }
+}
---
+++ hometasks/src/test/java/aqa_hw_9/ReviewVerificationTest.java
@@ -0,0 +1,30 @@
+package aqa_hw_9;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static java.lang.Thread.sleep;
+
+public class ReviewVerificationTest extends BaseTest{
+
+    @Test
+    public void verifySearchReview() throws InterruptedException {
+
+        String expectedReviewField1 = "Відгуки покупців";
+        String expectedReviewField2 = "Відгуки про інтернет-магазини";
+
+        HomePage homePage = new HomePage(getDriver());
+        homePage.toReviewPage();
+
+        ReviewPage reviewPage = new ReviewPage(getDriver());
+
+        String actualReviewField1 = reviewPage.getFieldContent("[class='reviews__title m_b-15']");
+        Assert.assertEquals(actualReviewField1, expectedReviewField1);
+
+        reviewPage.toReviewOfStories();
+        sleep(4000);
+
+        String actualReviewField2 = reviewPage.getFieldContent("h1.reviews__title");
+        Assert.assertEquals(actualReviewField2, expectedReviewField2);
+    }
+}
