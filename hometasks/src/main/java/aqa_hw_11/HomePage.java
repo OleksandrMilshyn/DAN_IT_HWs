a116aae666cb5c40dc821f2109f5d5d6dc24268d
[username] <[email]>
2025-09-14 20:37:52 +0200

    start hw

--- hometasks/src/main/java/aqa_hw_11/HomePage.java
+++ hometasks/src/main/java/aqa_hw_11/HomePage.java
@@ -1,6 +1,20 @@
 package aqa_hw_11;
 
+import io.qameta.allure.Step;
+
+import static com.codeborne.selenide.Selenide.$;
+
 public class HomePage {
 
-    public void toFilterPage(){}
+    @Step("User enters some word on search field ")
+    public void searchField(String text) {$("#story").setValue(text);}
+
+    @Step("User clicks on search Button")
+    public void searchButton() {$("button.search-btn").click();}
+
+    @Step("User clicks on some icon")
+    public void filmIcon() {$("div.short-mask").click();}
+
+    @Step("User clicks to series icon")
+    public void toFilterPage(){$("[href='/series/']").click();}
 }
